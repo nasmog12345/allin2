500f3163aa04162ffd68ec6f936b73e5a53dae94
nasmog12345 <126059506+nasmog12345@users.noreply.github.com>
2023-07-04 20:13:40 +0200

    erweiterung math. berechnung

--- frontend/src/views/Workouts.vue
+++ frontend/src/views/Workouts.vue
@@ -10,6 +10,10 @@
           Wiederholungen: {{ session.rep }}
           <br />
           Übung: {{ session.exercise }}
+          <br />
+          Letzte Dauer-Differenz: {{ getLastDurationDifference(session) }} min
+          <br />
+          Letzte Wiederholungs-Differenz: {{ getLastRepDifference(session) }}
         </ion-label>
       </ion-item>
     </ion-list>
@@ -21,11 +25,18 @@ import { IonContent, IonList, IonItem, IonLabel } from "@ionic/vue";
 import { defineComponent, ref, onMounted } from "vue";
 import axios from "axios";
 
+interface TrainingSession {
+  id: string;
+  trainingDate: string;
+  trainingDurationMinutes: number;
+  rep: number;
+  exercise: string;
+}
+
 export default defineComponent({
   components: { IonContent, IonList, IonItem, IonLabel },
   setup() {
-    // Erstellen Sie eine Inititalisierungsstruktur
-    const sessionInit = {
+    const sessionInit: TrainingSession = {
       id: "",
       trainingDate: "",
       trainingDurationMinutes: 0,
@@ -33,24 +44,73 @@ export default defineComponent({
       exercise: "",
     };
 
-    // Fügen Sie Initialisierungsstruktur zur ref hinzu
-    const trainingSessions = ref([sessionInit]);
+    const trainingSessions = ref<TrainingSession[]>([sessionInit]);
 
     const fetchTrainingSessions = async () => {
       try {
-        const response = await axios.get("http://localhost:8080/api/sessions", {
-          withCredentials: true,
-        });
+        const response = await axios.get<TrainingSession[]>(
+          "http://localhost:8080/api/sessions",
+          {
+            withCredentials: true,
+          }
+        );
         trainingSessions.value = response.data;
       } catch (error) {
         console.error(error);
       }
     };
 
+    const getLastDurationDifference = (session: TrainingSession) => {
+      const sessions = trainingSessions.value;
+      const exerciseSessions = sessions.filter(
+        (s) => s.exercise === session.exercise
+      );
+
+      if (exerciseSessions.length >= 2) {
+        const currentSessionIndex = exerciseSessions.findIndex(
+          (s) => s.id === session.id
+        );
+        if (currentSessionIndex > 0) {
+          const previousSession = exerciseSessions[currentSessionIndex - 1];
+          if (previousSession.exercise === session.exercise) {
+            return (
+              session.trainingDurationMinutes -
+              previousSession.trainingDurationMinutes
+            );
+          }
+        }
+      }
+
+      return null;
+    };
+
+    const getLastRepDifference = (session: TrainingSession) => {
+      const sessions = trainingSessions.value;
+      const exerciseSessions = sessions.filter(
+        (s) => s.exercise === session.exercise
+      );
+
+      if (exerciseSessions.length >= 2) {
+        const currentSessionIndex = exerciseSessions.findIndex(
+          (s) => s.id === session.id
+        );
+        if (currentSessionIndex > 0) {
+          const previousSession = exerciseSessions[currentSessionIndex - 1];
+          if (previousSession.exercise === session.exercise) {
+            return session.rep - previousSession.rep;
+          }
+        }
+      }
+
+      return null;
+    };
+
     onMounted(fetchTrainingSessions);
 
     return {
       trainingSessions,
+      getLastDurationDifference,
+      getLastRepDifference,
     };
   },
 });
