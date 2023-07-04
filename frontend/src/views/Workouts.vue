a28171b393711be13cacc2623dc3a7699031f95e
nasmog12345 <126059506+nasmog12345@users.noreply.github.com>
2023-07-04 16:56:00 +0200

    postreq

--- frontend/src/views/Workouts.vue
+++ frontend/src/views/Workouts.vue
@@ -1,28 +1,35 @@
 <template>
   <ion-list>
-    <ion-item>
-      <ion-label>Pokémon Yellow</ion-label>
-    </ion-item>
-    <ion-item>
-      <ion-label>Mega Man X</ion-label>
-    </ion-item>
-    <ion-item>
-      <ion-label>The Legend of Zelda</ion-label>
-    </ion-item>
-    <ion-item>
-      <ion-label>Pac-Man</ion-label>
-    </ion-item>
-    <ion-item>
-      <ion-label>Super Mario World</ion-label>
+    <ion-item v-for="trainingSession in trainingSessions" :key="trainingSession.id">
+      <ion-label>{{ trainingSession.exercise }}</ion-label>
     </ion-item>
   </ion-list>
 </template>
 
 <script lang="ts">
 import { IonItem, IonLabel, IonList } from "@ionic/vue";
-import { defineComponent } from "vue";
+import { defineComponent, ref, onMounted } from "vue";
+import axios from 'axios';
 
 export default defineComponent({
   components: { IonItem, IonLabel, IonList },
+  setup() {
+    const trainingSessions = ref([]);
+
+    const fetchTrainingSessions = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/api/sessions');
+        trainingSessions.value = response.data;
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    onMounted(fetchTrainingSessions);
+
+    return {
+      trainingSessions
+    };
+  },
 });
 </script>
