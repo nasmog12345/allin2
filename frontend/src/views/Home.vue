6290669f3ec17ebe640da2137c10eb0d948bb1d1
nasmog12345 <126059506+nasmog12345@users.noreply.github.com>
2023-07-05 10:51:48 +0200

    new push

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,50 +1,51 @@
 <template>
-  <ion-page>
-    <ion-content>
-      <ion-header>
-        <ion-toolbar>
-          <ion-title>Neues Workout</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      <ion-item>
-        <ion-label position="floating">Training Date</ion-label>
-        <br />
-        <ion-button v-if="!showDatePicker" @click="showDatePicker = true"
-          >Wähle Datum</ion-button
-        >
-        <ion-datetime
-          v-else
-          v-model="trainingDate"
-          display-format="DD-MMM-YYYY"
-          placeholder="Select Date"
-        ></ion-datetime>
-      </ion-item>
+  <ion-content>
+    <ion-card>
+      <ion-card-header>
+        <ion-card-title class="ion-text-center">Training Session</ion-card-title>
+      </ion-card-header>
 
-      <ion-item>
-        <ion-label position="floating">Duration (minutes)</ion-label>
-        <ion-input type="number" v-model="duration"></ion-input>
-      </ion-item>
+      <ion-card-content>
+        <ion-item>
+          <ion-label position="floating">Training Date</ion-label>
+          <br />
+          <ion-button v-if="!showDatePicker" @click="showDatePicker = true">
+            Wähle Datum
+          </ion-button>
+          <ion-datetime
+            v-else
+            v-model="trainingDate"
+            display-format="DD-MMM-YYYY"
+            placeholder="Select Date"
+          ></ion-datetime>
+        </ion-item>
 
-      <ion-item>
-        <ion-label position="floating">Repetitions</ion-label>
-        <ion-input type="number" v-model="repetitions"></ion-input>
-      </ion-item>
+        <ion-item>
+          <ion-label position="floating">Duration (minutes)</ion-label>
+          <ion-input type="number" v-model="duration"></ion-input>
+        </ion-item>
 
-      <ion-item>
-        <ion-label>Exercise Name</ion-label>
-        <ion-select v-model="exerciseName">
-          <ion-select-option
-            v-for="exercise in exercises"
-            :value="exercise"
-            :key="exercise"
-            >{{ exercise }}</ion-select-option
-          >
-        </ion-select>
-      </ion-item>
+        <ion-item>
+          <ion-label position="floating">Repetitions</ion-label>
+          <ion-input type="number" v-model="repetitions"></ion-input>
+        </ion-item>
 
-      <ion-button @click="postTrainingSession">Submit</ion-button>
-    </ion-content>
-  </ion-page>
+        <ion-item>
+          <ion-label>Exercise Name</ion-label>
+          <ion-select v-model="exerciseName" interface="action-sheet">
+            <ion-select-option
+              v-for="exercise in exercises"
+              :value="exercise"
+              :key="exercise"
+              >{{ exercise }}</ion-select-option
+            >
+          </ion-select>
+        </ion-item>
+
+        <ion-button expand="full" @click="postTrainingSession">Submit</ion-button>
+      </ion-card-content>
+    </ion-card>
+  </ion-content>
 </template>
 
 <script lang="ts">
@@ -55,12 +56,15 @@ import {
   IonLabel,
   IonInput,
   IonSelect,
-  IonPage,
   IonSelectOption,
   IonButton,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
 } from "@ionic/vue";
 import { defineComponent, ref } from "vue";
-import axios from "axios"; // Importieren von Axios
+import axios from "axios";
 
 export default defineComponent({
   components: {
@@ -69,13 +73,16 @@ export default defineComponent({
     IonItem,
     IonLabel,
     IonInput,
-    IonPage,
     IonSelect,
     IonSelectOption,
     IonButton,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
   },
   setup() {
-    const trainingDate = ref("");
+    const trainingDate = ref(null);
     const showDatePicker = ref(false);
     const duration = ref(0);
     const repetitions = ref(0);
@@ -130,8 +137,23 @@ export default defineComponent({
       repetitions,
       exerciseName,
       exercises,
-      postTrainingSession, // Sie können diese Funktion dann verwenden, um die aktuelle Trainingssession zu posten
+      postTrainingSession,
     };
   },
 });
 </script>
+
+<style scoped>
+ion-content {
+  --ion-background-color: #f4f4f4;
+}
+
+ion-card {
+  max-width: 400px;
+  margin: 0 auto;
+}
+
+ion-button[expand="full"] {
+  margin-top: 20px;
+}
+</style>
